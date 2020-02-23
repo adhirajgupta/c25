29085caf91191b5c6077e1728fc1af991af9b15e
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2020-02-23 11:01:47 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,7 @@
+class Box extends BaseClass{
+  constructor(x,y,width,height){
+    super(x,y,width,height);
+    this.image = loadImage("D:\CODING CLASS\visual studio code\c25\angryBirdsStage2StudentActivity-master\sprites/wood1.png");
+  }
+ }
+
---
+++ Pig.js
@@ -0,0 +1,8 @@
+
+  class Pig extends BaseClass{
+    constructor(x,y,width,height){
+      super(x,y,width,height);
+      this.image = loadImage("D:\CODING CLASS\visual studio code\c25\angryBirdsStage2StudentActivity-master\sprites/wood1.png");
+    }
+   }
+
